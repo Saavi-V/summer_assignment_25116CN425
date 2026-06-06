fc396b3e00320b1764feff1977fe1231f1cf51eb
Saavi-V <shortterm.2024@gmail.com>
2026-06-06 20:22:26 +0530

    Create Day_2/day2_Q5.c
    Day2_Q5 = Find sum of digits of a number

---
+++ Day_2/day2_Q5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){  
+    int num,rem,sum=0;
+    printf("Enter any number\n");
+    scanf("%d",&num);
+    while(num>0){
+        rem=num%10;
+        sum+=rem;
+        num=num/10;
+    }
+    printf("Sum of digits of a number %d",sum);
+    return 0;
+}
