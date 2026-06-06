e07863e902c3411e9606f31d9032ab7cdc6e11ca
Saavi-V <shortterm.2024@gmail.com>
2026-06-06 21:49:51 +0530

    Create Day_3/day3_Q9.c
    Day3_Q9 = Check whether a number is prime

---
+++ Day_3/day3_Q9.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int num,i;
+    printf("Enter any number\n");
+    scanf("%d",&num);
+    if(num<=1){
+        printf("Not prime");
+        return 1;
+    }    
+    for(i=2; i<num ;i++){
+         if (num%i==0){
+            printf("Not Prime"); 
+            return 2;
+         }   
+    }
+    printf("Prime");
+    return 0;
+}
