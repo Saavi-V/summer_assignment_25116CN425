4f89e1cca9552dbec3062f5a9efba9061ff39b2a
Saavi-V <shortterm.2024@gmail.com>
2026-06-13 01:46:46 +0530

    Update day05_Q18.c
    Day05_Q18 = Check strong number

--- Day_05/day05_Q18.c
+++ Day_05/day05_Q18.c
@@ -1,1 +1,23 @@
-bnn
+#include<stdio.h>
+int main(){
+    int num,onum,i,rem,fact=1,sum=0;
+    printf("Enter any number\n");
+    scanf("%d",&num);
+    onum=num;
+    printf("The sum of factorial of digits is\n");
+    while(num>0){
+         rem=num%10;
+         for(i=1;i<=rem;i++){
+             fact*=i;
+         }
+         sum+=fact;
+         num=num/10;
+         printf("%d ",fact);
+         if(num>0) printf("+ ");
+         fact=1;
+    }
+    printf("= %d\n",sum);
+    if(onum==sum) printf("\n%d is strong number",onum); 
+    else printf("Entered number is not a strong number");
+    return 0;
+}
