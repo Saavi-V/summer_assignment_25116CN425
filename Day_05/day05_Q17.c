b9d7c98a769e5bd0694e56c0345a8e8513e91fa8
Saavi-V <shortterm.2024@gmail.com>
2026-06-13 01:16:12 +0530

    Update day05_Q17.c
    Day05_Q17 = Check perfect number

--- Day_05/day05_Q17.c
+++ Day_05/day05_Q17.c
@@ -1,1 +1,19 @@
-fghhj
+#include<stdio.h>
+int main()
+{
+    int num,i,sum=0;
+    printf("Enter a number\n");
+    scanf("%d",&num);
+    if(num<=0){
+        printf("Enter a positive number");
+        return 1;
+    }
+    for(i=1;i<=num/2;i++){
+         if(num%i==0){
+             sum+=i;
+         }
+    }
+    if(num==sum) printf("%d is a perfect number",num);
+    else printf("%d is not a perfect number",num);
+    return 0;
+}
