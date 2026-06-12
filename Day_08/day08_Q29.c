f159d3cdbe4ac44835640e0cd7e9c4776435a6c0
Saavi-V <shortterm.2024@gmail.com>
2026-06-13 01:53:38 +0530

    Update day08_Q29.c
    Day08_Q29 = Half pyramid pattern

--- Day_08/day08_Q29.c
+++ Day_08/day08_Q29.c
@@ -1,1 +1,13 @@
-dhjk
+#include<stdio.h>
+int main(){
+    int i,j,h;
+    printf("Enter height width of triangle\n");
+    scanf("%d",&h);
+    for(i=0;i<h+1;i++){
+        for(j=0;j<i;j++){
+            printf("*\t");
+        }
+        printf("\n");
+    }
+    return 0;
+}
